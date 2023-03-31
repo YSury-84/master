2274dd4a8cfdece6466601cfe0172dcd77b6dc43
YSury-84 <Yury.Kolchurin@evraz.com>
2023-03-31 14:05:15 +0700

    Module 3, v1.0

---
+++ Module3+/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Module3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+
+            Console.ReadKey();
+        }
+    }
+
+    enum DaysOfWeek : byte
+    {
+        Tuesday = 1,
+        Monday,
+        Wednesday,
+        Friday
+    }
+
+}
