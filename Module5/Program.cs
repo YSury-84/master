b583f7db81c431c1165ad70cef21e5f0d526ac4e
YSury-84 <Yury.Kolchurin@evraz.com>
2023-04-18 16:18:40 +0700

    Модуль 5 - Фиксация кода на сервере, согласно задания 5.5.3

--- Module5/Program.cs
+++ Module5/Program.cs
@@ -4,128 +4,21 @@
     {
         static void Main(string[] args)
         {
-            public static void Main(string[] args)
+            static int Factorial(int x)
             {
-                Console.WriteLine("Цикл for");
-
-                for (int i = 0; i < 3; i++)
+                if (x == 0)
                 {
-                    Console.WriteLine(i);
-
-                    Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-                    var color = Console.ReadLine();
-
-                    switch (color)
-                    {
-                        case "red":
-                            Console.BackgroundColor = ConsoleColor.Red;
-                            Console.ForegroundColor = ConsoleColor.Black;
-
-                            Console.WriteLine("Your color is red!");
-                            break;
-
-                        case "green":
-                            Console.BackgroundColor = ConsoleColor.Green;
-                            Console.ForegroundColor = ConsoleColor.Black;
-
-                            Console.WriteLine("Your color is green!");
-                            break;
-                        case "cyan":
-                            Console.BackgroundColor = ConsoleColor.Cyan;
-                            Console.ForegroundColor = ConsoleColor.Black;
-
-                            Console.WriteLine("Your color is cyan!");
-                            break;
-                        default:
-                            Console.BackgroundColor = ConsoleColor.Yellow;
-                            Console.ForegroundColor = ConsoleColor.Red;
-
-                            Console.WriteLine("Your color is yellow!");
-                            break;
-                    }
+                    return 1;
                 }
-
-                Console.WriteLine("Цикл while");
-                int k = 0;
-                while (k < 3)
+                else
                 {
-                    Console.WriteLine(k);
-
-                    Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-                    var color = Console.ReadLine();
-
-                    switch (color)
-                    {
-                        case "red":
-                            Console.BackgroundColor = ConsoleColor.Red;
-                            Console.ForegroundColor = ConsoleColor.Black;
-
-                            Console.WriteLine("Your color is red!");
-                            break;
-
-                        case "green":
-                            Console.BackgroundColor = ConsoleColor.Green;
-                            Console.ForegroundColor = ConsoleColor.Black;
-
-                            Console.WriteLine("Your color is green!");
-                            break;
-                        case "cyan":
-                            Console.BackgroundColor = ConsoleColor.Cyan;
-                            Console.ForegroundColor = ConsoleColor.Black;
-
-                            Console.WriteLine("Your color is cyan!");
-                            break;
-                        default:
-                            Console.BackgroundColor = ConsoleColor.Yellow;
-                            Console.ForegroundColor = ConsoleColor.Red;
-
-                            Console.WriteLine("Your color is yellow!");
-                            break;
-                    }
+                    return x * Factorial(x - 1);
                 }
-
-                Console.WriteLine("Цикл do");
-                int t = 0;
-
-                do
-                {
-                    Console.WriteLine(t);
-
-                    Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-                    var color = Console.ReadLine();
-
-                    switch (color)
-                    {
-                        case "red":
-                            Console.BackgroundColor = ConsoleColor.Red;
-                            Console.ForegroundColor = ConsoleColor.Black;
-
-                            Console.WriteLine("Your color is red!");
-                            break;
-
-                        case "green":
-                            Console.BackgroundColor = ConsoleColor.Green;
-                            Console.ForegroundColor = ConsoleColor.Black;
-
-                            Console.WriteLine("Your color is green!");
-                            break;
-                        case "cyan":
-                            Console.BackgroundColor = ConsoleColor.Cyan;
-                            Console.ForegroundColor = ConsoleColor.Black;
-
-                            Console.WriteLine("Your color is cyan!");
-                            break;
-                        default:
-                            Console.BackgroundColor = ConsoleColor.Yellow;
-                            Console.ForegroundColor = ConsoleColor.Red;
-
-                            Console.WriteLine("Your color is yellow!");
-                            break;
-                    }
-
-                } while (t < 3);
-
-                Console.ReadKey();
             }
+
+            Console.WriteLine(Factorial(20));
+
+            Console.ReadLine();
+        }
     }
 }
