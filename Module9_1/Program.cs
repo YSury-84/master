9c5e2dec8fd34dcacb72834e4e50c8c6efe65221
YSury-84 <Yury.Kolchurin@evraz.com>
2023-05-13 21:18:13 +0700

    Выполнено итоговое задание Модуля 9.

---
+++ Module9_1/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Module9_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер исключения:");
+            string n = Console.ReadLine();
+            int i = 0;
+            try
+            {
+                i = Convert.ToInt32(n);
+            }
+            catch
+            {
+                i = 0;
+            }
+            Exception[] myex = new Exception[5];
+            myex[0] = new MyError("Ошибка: Мое собственное исключение!");
+            myex[1] = new Exception("Исключение #1");
+            myex[2] = new Exception("Исключение #2");
+            myex[3] = new Exception("Исключение #3");
+            myex[4] = new Exception("Исключение #4");
+            myex[1].HelpLink = "www.google.com";
+            myex[1].HResult = 1;
+            myex[2].HResult = 2;
+            myex[3].HResult = 3;
+            myex[4].HResult = 4;
+
+            try
+            {
+                if (i >= 1 && i <= 4)
+                {
+                    throw myex[i];
+                }
+                else
+                {
+                    throw myex[0];
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Произошло исключение: " + myex[i].Message);
+            }
+        }
+
+        public class MyError : ArgumentException
+        {
+            public MyError(string _exeptionMessege) : base(_exeptionMessege)
+            { }
+        }
+    }
+}
---
+++ Module9_2/Program.cs
@@ -0,0 +1,80 @@
+﻿using static Module9_2.Program;
+
+namespace Module9_2
+{
+    internal class Program
+    {
+        static class Pipl
+        {
+            public static List<string> people = new List<string>() { "Петров", "Сидоров", "Иванов", "Медведев", "Шариков" };
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ProcessBusinessLogic blTSort = new ProcessBusinessLogic();
+            blTSort.TSort += bl_TSort; // регистрируем событие
+            ProcessBusinessLogic blFSort = new ProcessBusinessLogic();
+            blFSort.FSort += bl_FSort; // регистрируем событие
+            while (true)
+            {
+                Console.Write("Введите команду: ");
+                string s = Console.ReadLine();
+                if (s == "exit") break;
+                else
+                if (s == "1") { blTSort.StartSort(true); }
+                else
+                if (s == "2") { blFSort.StartSort(false); }
+                else
+                { Console.Write("Команда не распознана!"); }
+            }
+        }
+
+        public delegate void Notify();  // делегат
+        public class ProcessBusinessLogic
+        {
+            public event Notify TSort; // событие
+            public event Notify FSort; // событие
+
+            public void StartSort(bool bSort)
+            {
+                if (bSort)
+                { TSort(); }
+                else
+                { FSort(); }
+            }
+
+            protected virtual void OnTSort()
+            {
+                TSort?.Invoke();
+            }
+            protected virtual void OnFSort() 
+            {
+                FSort?.Invoke();
+            }
+        }
+
+        // перехватчик событий
+        public static void bl_TSort()
+        {
+            Console.WriteLine("Sort is True");
+            Pipl.people.Sort();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(Pipl.people[i]);
+            }
+        }
+
+        // перехватчик событий
+        public static void bl_FSort()
+        {
+            Console.WriteLine("Sort is False");
+            Pipl.people.Sort();
+            for (int i = 4; i >=0; i--)
+            {
+                Console.WriteLine(Pipl.people[i]);
+            }
+        }
+
+    }
+}
